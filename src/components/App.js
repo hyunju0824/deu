5029137bb822b473ab712f80ee398718a5f8a3c7
정현주 <hyunjuicy@jeonghyeonjuui-MacBookPro.local>
2024-02-19 19:00:32 +0900

    테일윈드

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css';
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path='/' element={<></>} />
+          <Route path='/' element={<Main/>} />
         </Routes>
     </div>
   );
